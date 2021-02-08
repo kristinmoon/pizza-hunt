35f1065d18b784453405e2a8e702966659da6ca4
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2021-02-08 00:12:38 -0700

    user routes

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,65 @@
+const { Pizza } = require('../models');
+
+const pizzaController = {
+  // GET all pizzas /api/pizzas
+  getAllPizza(req, res) {
+    Pizza.find({})
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // GET one pizza by id
+  getPizzaById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .then(dbPizzaData => {
+        // If no pizza is found, send 404
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // POST/create Pizza /api/pizzas
+  createPizza({ body }, res) {
+    Pizza.create(body)
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  // PUT/update pizza by id /api/pizzas/:id
+  updatePizza({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  // DELETE pizza
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.status(400).json(err));
+  }
+}
+
+module.exports = pizzaController;
